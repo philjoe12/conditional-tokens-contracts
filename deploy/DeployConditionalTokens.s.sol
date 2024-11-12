2808e5fa9447e5d525c96a5179c33fd54352400a
philljoe12 <philljoe12@gmail.com>
2024-11-12 08:32:31 -0800

    Save current changes before installing forge-std

---
+++ deploy/DeployConditionalTokens.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "forge-std/Script.sol";
+import "../src/ConditionalTokens.sol";
+
+contract DeployConditionalTokens is Script {
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PK");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Deploy the ConditionalTokens contract
+        ConditionalTokens conditionalTokens = new ConditionalTokens();
+
+        console.log("ConditionalTokens deployed at:", address(conditionalTokens));
+        vm.stopBroadcast();
+    }
+}
--- test/MockCoin.sol
+++ test/MockCoin.sol
@@ -1,4 +1,19 @@
-pragma solidity ^0.5.1;
-import { ERC20Mintable } from "@openzeppelin/contracts/token/ERC20/ERC20Mintable.sol";
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-contract MockCoin is ERC20Mintable {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract MockCoin is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC20("MockCoin", "MCK") {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _setupRole(MINTER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public {
+        require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+        _mint(to, amount);
+    }
+}
